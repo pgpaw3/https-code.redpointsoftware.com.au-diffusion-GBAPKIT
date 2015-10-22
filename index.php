436d55071b76d5aaf04d55614bd61a5dfd1c83eb
pgpaw3 <2070863@student.swin.edu.au>
2015-10-22 22:41:57 +1100

    Backup Test

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Goblin Darts - Home</title>
+<style type="text/css"></style>
+<link href="Page2.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+a:link {
+	color: #000;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+	color: #F90;
+}
+a:active {
+	text-decoration: none;
+}
+body {
+	background-color: #FFFFFF;
+	background-image: url();
+}
+#Table {
+	color: #FFF; 
+}
+#Table {
+	color: #FFF;
+}
+#Container #Header table tr th {
+	color: #FFF;
+}
+#Table Pos {
+	text-align: right;
+}
+#Container #Header table {
+	text-align: right;
+}
+#Container #Center p {
+	font-size: 16px;
+	padding-right: 20px;
+	padding-left: 20px;
+}
+</style>
+</head>
+<body onload="MM_preloadImage('images/header.png','images/all classes_v2.png','images/Ice HUD PROF.png','images/FIRE MEHUD PROF.png','images/EARTH HUD PROF.png','images/LIGHTNING HUD PROF.png','images/DARK HUD PROF.png','images/LIGHT HUD PROF.png')">
+<div id="Container">
+  <div id="Header"><img src="images/header.png" width="1000" height="200" alt="Header" /></div>
+  <div id="Left">
+    <p>dnfdsjgjfg    
+    <p><strong>Factsheet</strong>
+    <p>Developer: Goblin Darts    </p>
+    <p>Melbourne, Australia</p>
+    <br><strong>Founding Date:</strong></br>
+    <p>March, 2015</p>
+    <br><strong>Current Projects:</strong></br>
+    <p>We Go Forth</p>
+    <p>&nbsp;</p>
+  </div>
+  <div id="Center">
+    <p dir="ltr"><strong>The Game</strong></p>
+    <p dir="ltr">We Go Forth is a four player cooperative dungeon crawler that puts the player in a party of four with the choice of six classes, each with a unique play style and ability set. </p>
+    <p dir="ltr">Players take control of one of six elemental entities long grown bored of their mastery of the natural order. They seek a new challenge, and find their way to the Formless Place, a dimension of challenge, struggle and triumph. Communication and teamwork is impertive, as without it they will never return alive.</p>
+    <p dir="ltr">Learn from each successive playthrough how best to form your party, combat tactical situations and conquer each dungeon with the goal of pushing your party further into the the Formless Place than ever before. Gather your party, go forth and triumph.</p>
+    <p dir="ltr">&nbsp;</p>
+    <p dir="ltr"><strong>Game Features</strong><br />
+</p>
+    <ul>
+      <li dir="ltr">Randomized dungeons: Play dungeons built out of a variety of handcrafted level pieces. 
+      </li>
+    </ul>
+    <ul>
+      <li> Four player local co-op: Invite your friends over for a challenging and frantic co-op experience. </li>
+    </ul>
+    <ul>
+      <li dir="ltr">6 classes: Choose from six classes all with radically different playstyles and abilities</li>
+    </ul>
+    <ul>
+      <li>8 enemy types: Fight against 8 enemy types in challenging tactical environments.</li>
+    </ul>
+    <ul>
+      <li dir="ltr">Celtic inspired fantasy world: Challenge the formless place a nether realm beyond the comprehension of the natural order of the world.</li>
+    </ul>
+    <p>&nbsp;</p>
+    <p dir="ltr"><strong>Platform</strong></p>
+    <p dir="ltr">The current aim of We Go Forth is for PC with the Unity engine, however there is also the possibility of an Xbox Live Arcade version, and further platforms beyond that in the future.</p>
+    <p dir="ltr">&nbsp;</p>
+    <p dir="ltr"><strong>Meet Your Party</strong></p>
+    <img src="images/all classes_v2.png" width="700" height="300" alt="The playable characters" /><br />
+    <p dir="ltr"><strong>Sian:</strong> </p>
+    <p dir="ltr"><img src="images/Ice HUD PROF.png" width="256" height="256" alt="Sian" /></p>
+    <ul>
+      <li dir="ltr">Dignified, composed and not fond of being hit, Sian is the master of winter. </li>
+    </ul>
+    <ul>
+      <li dir="ltr">As a warrior Sian chooses to fight from a distance rather than up close, cool, collected and intelligent.    </li>
+    </ul>
+    <p dir="ltr"><strong>Fogamur:</strong></p>
+    <p dir="ltr"><img src="images/FIRE MEHUD PROF.png" width="256" height="256" alt="Fogamur" /></p>
+    <ul>
+      <li> Aggressive and fluid, Fogamur is the master of flame, a harbinger of destruction. </li>
+    </ul>
+    <ul>
+      <li>As a warrior Fogamur fights up close aggressively causing massive damage.    </li>
+    </ul>
+    <p dir="ltr"><strong>Trithilon:</strong></p>
+    <p dir="ltr"><img src="images/EARTH HUD PROF.png" width="256" height="256" alt="Trithilon" /></p>
+    <ul>
+      <li dir="ltr">Quiet and gentle, Trithilon is the master of the stones and trees, a vigilant warden. </li>
+    </ul>
+    <ul>
+      <li dir="ltr">Trithilon as a warrior commands the field protecting and defending party members.    </li>
+    </ul>
+    <p dir="ltr"><strong>Empyrean:</strong></p>
+    <p dir="ltr"><img src="images/LIGHTNING HUD PROF.png" width="256" height="256" alt="Empyrean" /></p>
+    <ul>
+      <li dir="ltr">Feral and slight, Empyrean is master of the storm, fleet of foot bringing a silent devastation upon foes. </li>
+    </ul>
+    <ul>
+      <li dir="ltr">As a warrior Empyrean stays out of trouble flanking and manoeuvring to strike with great damage from behind only to disappear once more.    </li>
+    </ul>
+    <p dir="ltr"><strong>Noethe:</strong></p>
+    <p dir="ltr"><img src="images/DARK HUD PROF.png" width="256" height="256" alt="Noethe" /></p>
+    <ul>
+      <li dir="ltr">Pensive and Consuming, Noethe is master of the night. Still and oppressive, the dark consumes all.</li>
+    </ul>
+    <ul>
+      <li dir="ltr">As a warrior Noethe sacrifices speed for power, devastating whilst stationary but fragile on the move.    </li>
+    </ul>
+    <p dir="ltr"><strong>Dydd:</strong></p>
+    <p dir="ltr"><img src="images/LIGHT HUD PROF.png" width="256" height="256" alt="Dydd" /></p>
+    <ul>
+      <li dir="ltr">Regal and commanding, Dydd is master of sunlight, standing above all the light purges, demanding  the gaze of all before it.</li>
+    </ul>
+    <ul>
+      <li dir="ltr">As a warrior Dydd protects commands and buffs its party, powerful alone but all the more powerful for its allies.</li>
+    </ul>
+    <p>&nbsp;</p>
+    <p><strong>Images:</strong></p>
+    <table width="200" border="0">
+      <tr>
+        <td><img src="images/logo.png" width="512" height="245" alt="We Go Forth Logo" /></td>
+        <td><img src="images/soooul_keeper_720.png" width="390" height="250" alt="Soul Keeper concept art" /></td>
+      </tr>
+      <tr>
+        <td><img src="images/swarmer concept.jpg" width="500" height="500" alt="Swamer Concept Art" /></td>
+        <td><img src="images/GATE KEEPER.png" width="400" height="248" alt="Gate Keeper" /></td>
+      </tr>
+      <tr>
+        <td><img src="images/classes 1-3.png" width="500" alt="Earth, Ice and Fire Classes" /></td>
+        <td><img src="images/classes4-6.png" width="500" height="210" alt="Dark, Lightning and Light classes" /></td>
+      </tr>
+      <tr>
+        <td><img src="images/character_sheet_720.png" width="509" height="720" alt="Character Concept art" /></td>
+        <td><img src="images/build types.jpg" width="450" height="174" alt="Charater Build Guides" /></td>
+      </tr>
+    </table>
+    <p>&nbsp;</p>
+    <p><strong>Meet the team at  Goblin Darts</strong></p>
+    <p>Goblin  Darts is a small Melbourne based video game developer. Over the course of 2015  we have been painstakingly developing our first video game, We Go Forth, a 4 player co-operative dungeon crawler.</p>
+    <p>Michael Loorham: Lead  Artist <br />
+email: 9955585@student.swin.edu.au </p>
+    <p>Ashley Wood: Lead Animator/3D  Modeller <br />
+email: 2070863@student.swin.edu.au</p>
+    <p>Jeffrey Ng: Lead  Character Modeller <br />
+email: 1793780@student.swin.edu.au </p>
+    <p>Lachlan Goodear:  Artist/3D Modeller <br />
+email:  9725636@student.swin.edu.au </p>
+    <p>Liam Dahl: Character  Rigging/3D Modeller <br />
+email: 726593x@student.swin.edu.au </p>
+    <p>Aidan Lee:  Programmer/Particle Effects Guru <br />
+email: 9526668@student.swin.edu.au  &nbsp; </p>
+    <p>Daniel Draper:  Programmer <br />
+email: 7380348@student.swin.edu.au </p>
+    <p>Lewis Herald:  Programmer <br />
+email: 382235@student.swin.edu.au </p>
+    <p>&nbsp;</p>
+    <p><strong>Special Thanks</strong></p>
+    <p>Floating Islands - Fantasy Environment Pack <br />
+      3dfancy.com</br>
+    </p>
+    <p>Tom Fortune: 3D Modelling</p>
+    <p>Andy Trevillian: Tutoring</p>
+    <p>Clinton Woodward: Tutoring</p>
+    <p>Giulia Farag: Music Composition</p>
+    <p>Michael Oliverio: Music Composition</p>
+    <p>David Brownell: Music Composition  </p>
+    <p>Matt Ivancic: Music Composition</p>
+  </div>
+  <div id="Footer">© 2015 Goblin Darts. 
+    <script language="Javascript">
+document.write("This page was last edited on: " + document.lastModified +"");
+</SCRIPT>
+  </div>
+</div>
+</body>
+</html>
